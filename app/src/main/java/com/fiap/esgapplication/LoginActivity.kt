b2cfedd4ddf1d6f7719d58af43659306276d6ab0
ericaokamura <erica.okamura@yahoo.com.br>
2023-08-25 09:29:30 -0300

    primeiro commit

---
+++ app/src/main/java/com/fiap/esgapplication/LoginActivity.kt
@@ -0,0 +1,43 @@
+package com.fiap.esgapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val btnOk = findViewById<Button>(R.id.button)
+        val edtUsuario = findViewById<EditText>(R.id.editText)
+        val edtSenha = findViewById<EditText>(R.id.editText2)
+
+        btnOk.setOnClickListener {
+            if (TextUtils.isEmpty(edtUsuario.editableText.toString())) {
+                Toast.makeText(this, "Campo Usuário é obrigatório!", Toast.LENGTH_LONG)
+                edtUsuario.setError("Campo Usuário é obrigatório!")
+                if (TextUtils.isEmpty(edtSenha.editableText.toString())) {
+                    Toast.makeText(this, "Campo Senha é obrigatório!", Toast.LENGTH_LONG)
+                    edtSenha.setError("Campo Senha é obrigatório!")
+                }
+            } else {
+                if (TextUtils.isEmpty(edtSenha.editableText.toString())) {
+                    Toast.makeText(this, "Campo Senha é obrigatório!", Toast.LENGTH_LONG)
+                    edtSenha.setError("Campo Senha é obrigatório!")
+                } else {
+                    val sendIntent = Intent(this, MainActivity::class.java)
+                    sendIntent.action = Intent.ACTION_SEND
+                    sendIntent.putExtra("usuario", edtUsuario.editableText.toString())
+                    sendIntent.type = "text/plain"
+                    startActivity(sendIntent)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/fiap/esgapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.fiap.esgapplication
+
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = getIntent();
+        val usuario = intent.getStringExtra("usuario")
+
+        val txtUsuario = findViewById<TextView>(R.id.textView4)
+        txtUsuario.text = "Olá " + usuario + "!"
+
+
+    }
+
+}
